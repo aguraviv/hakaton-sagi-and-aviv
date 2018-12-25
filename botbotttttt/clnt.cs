2a837bcc867d6cace0afaef023b9bb5b21513777
aguraviv <aguraviv12@gmail.com>
2018-12-25 19:56:10 +0200

    victom and bot connecting to it

---
+++ botbotttttt/clnt.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+
+public class clnt
+{
+
+    public static void Main()
+    {
+        /*
+        try
+        {
+            TcpClient tcpclnt = new TcpClient();
+            Console.WriteLine("Connecting.....");
+
+            tcpclnt.Connect("127.0.0.1", 8001);
+            // use the ipaddress as in the server program
+
+            Console.WriteLine("Connected");
+            Console.Write("Enter the string to be transmitted : ");
+
+            String str = Console.ReadLine();
+            Stream stm = tcpclnt.GetStream();
+
+            ASCIIEncoding asen = new ASCIIEncoding();
+            byte[] ba = asen.GetBytes(str);
+            Console.WriteLine("Transmitting.....");
+
+            stm.Write(ba, 0, ba.Length);
+
+            byte[] bb = new byte[100];
+            int k = stm.Read(bb, 0, 100);
+
+            for (int i = 0; i < k; i++)
+                Console.Write(Convert.ToChar(bb[i]));
+
+            Thread.Sleep(50000);
+            tcpclnt.Close();
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine("Error..... " + e.StackTrace);
+        }
+        */
+
+        now_we_hack("127.0.0.1", 8001, "1234567", "aviv&sagi the kings");
+    }
+
+
+
+    public static void now_we_hack(String ip, int port, String pw, String source_server)
+    {
+        try
+        {
+            TcpClient tcpclnt = new TcpClient();
+            Console.WriteLine("Connecting.....");
+
+            tcpclnt.Connect(ip, port);
+            // use the ipaddress as in the server program
+
+            Console.WriteLine("Connected");
+            Console.Write("Enter the string to be transmitted : ");
+
+            String str = pw;
+            Stream stm = tcpclnt.GetStream();
+
+            ASCIIEncoding asen = new ASCIIEncoding();
+            byte[] ba = asen.GetBytes(str);
+            Console.WriteLine("Transmitting.....");
+            
+            stm.Write(ba, 0, ba.Length);
+
+            byte[] bb = new byte[100];
+            int k = stm.Read(bb, 0, 100);
+            String sir1 = "";
+            for (int i = 0; i < k; i++)
+                sir1 = sir1 + Convert.ToChar(bb[i]);
+            if (sir1 == "access granted")
+            {
+                Console.WriteLine(sir1);
+                String str1 = "you have been hacked by aviv&sagigever";
+                Stream stm1 = tcpclnt.GetStream();
+
+                ASCIIEncoding asen1 = new ASCIIEncoding();
+                byte[] ba1 = asen1.GetBytes(str1);
+                stm1.Write(ba1, 0, ba1.Length);
+            }
+            
+
+            Thread.Sleep(50000);
+            tcpclnt.Close();
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine("Error..... " + e.StackTrace);
+        }
+    }
+
+}
---
+++ vistimmmmmm/serv.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections.Generic;
+
+public class serv
+{
+    public static List<DateTime> list_of_hacked = new List<DateTime>();
+
+
+    public static void add_to_list_and_check_her(String sir1)
+    {
+        list_of_hacked.Add(DateTime.Now);
+        Console.WriteLine("number of hacked is..." + list_of_hacked.Count);
+        if (list_of_hacked.Count >= 10)
+        {
+            
+            if((list_of_hacked[2] - list_of_hacked[0]).Seconds <1)
+            {
+                Console.WriteLine("you hacked me " + sir1);
+
+            }
+            else
+            {
+                list_of_hacked.RemoveAt(0);
+            }
+        }
+    }
+    public static void Main()
+    {
+        
+        string pasword = "1234567";
+        while (true)
+        {
+            try
+            {
+                IPAddress ipAd = IPAddress.Parse("127.0.0.1");
+                // use local m/c IP address, and 
+                // use the same in the client
+
+                /* Initializes the Listener */
+                TcpListener myList = new TcpListener(ipAd, 8001);
+
+                /* Start Listeneting at the specified port */
+                myList.Start();
+
+                Console.WriteLine("The server is running at port 8001...");
+                Console.WriteLine("The local End point is  :" +
+                                  myList.LocalEndpoint);
+                Console.WriteLine("Waiting for a connection.....");
+
+                Socket s = myList.AcceptSocket();
+                Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+                byte[] b = new byte[100];
+                int k = s.Receive(b);
+                Console.WriteLine("Recieved...");
+                String sir = "";
+                for (int i = 0; i < k; i++)
+                    sir = sir + Convert.ToChar(b[i]);
+                if (sir == pasword)
+                {
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    s.Send(asen.GetBytes("access granted"));
+                    //Console.WriteLine("\nSent Acknowledgement");
+                    byte[] b1 = new byte[100];
+                    int k1 = s.Receive(b1);
+                    Console.WriteLine("Recieved...");
+                    String sir1 = "";
+                    for (int i = 0; i < k1; i++)
+                        sir1 = sir1 + Convert.ToChar(b1[i]);
+                    add_to_list_and_check_her(sir1);
+                }
+                else
+                {
+                    Console.WriteLine(sir);
+
+
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    s.Send(asen.GetBytes("The string was recieved by the server."));
+                    Console.WriteLine("\nSent Acknowledgement");
+                }
+                /* clean up */
+                s.Close();
+                myList.Stop();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+        }
+    }
+
+}
